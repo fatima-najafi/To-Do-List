11851f9aa0fca6f7a252e5a0cc846b125dfa4fc2
Fatima Najafi <najafifatima762@gmail.com>
2023-07-12 01:10:02 +0430

    add list-structure and webpack for todo list project

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: path.resolve(__dirname, "./src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    filename: "[name].bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/template.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpeg|jpg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
