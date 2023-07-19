17604d4282cdc31db722e3fce227efb7dcbe8264
Fatima Najafi <najafifatima762@gmail.com>
2023-07-19 18:58:33 +0430

    add test for remove and add data in todo list

---
+++ src/test/todolist.test.js
@@ -0,0 +1,33 @@
+/* eslint-disable quotes */
+import addData from "../module/addData.js";
+import clearAllData from "../module/clearAllData.js";
+import deletTasksFromLocalSorage from "../module/deleteData.js";
+
+import getdata from "../module/getData.js";
+import testCheckBoxValue from "./testCheckData.js";
+
+describe("Test methods", () => {
+  test("should add data to the list", () => {
+    addData("the");
+    const data = getdata();
+    expect(data.length).toBe(1);
+  });
+
+  test("should check the checkbox of data from the list", () => {
+    testCheckBoxValue(true);
+    const data = getdata();
+    expect(data[0].completed).toBe(true);
+  });
+
+  test("should delete the checked data from the list", () => {
+    clearAllData();
+    const data = getdata();
+    expect(data.length).toBe(0);
+  });
+
+  test("should delete data from the list", () => {
+    deletTasksFromLocalSorage("hi");
+    const data = getdata();
+    expect(data.length).toBe(0);
+  });
+});
