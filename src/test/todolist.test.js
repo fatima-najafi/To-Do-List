e7e32972eb39e4c644f32f268b5bb8f2bc636acf
Fatima Najafi <najafifatima762@gmail.com>
2023-07-20 17:27:57 +0430

    test the upadate and clearall functions

--- src/test/todolist.test.js
+++ src/test/todolist.test.js
@@ -1,17 +1,25 @@
 /* eslint-disable quotes */
+import "jest-localstorage-mock";
 import addData from "../module/addData.js";
 import clearAllData from "../module/clearAllData.js";
 import deletTasksFromLocalSorage from "../module/deleteData.js";
+import { testCurrentValue, testEditValue } from "./testEditData.js";
+
 import getdata from "../module/getData.js";
 import testCheckBoxValue from "./testCheckData.js";
 
 describe("Test methods", () => {
   test("should add data to the list", () => {
-    addData("read-book");
+    addData("test todo list the second part");
     const data = getdata();
     expect(data.length).toBe(1);
   });
-
+  test("should edit the data from the list", () => {
+    testCurrentValue("test todo list the second part");
+    testEditValue("reading js");
+    const data = getdata();
+    expect(data[0].description).toBe("reading js");
+  });
   test("should check the checkbox of data from the list", () => {
     testCheckBoxValue(true);
     const data = getdata();
