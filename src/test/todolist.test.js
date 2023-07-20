eae8b230087c3fee5d2571bc78ceeceb64ecae06
Mehmet Selçuk Güler <gercekselcukguler@gmail.com>
2023-07-20 17:07:14 +0300

    Fix eslint errors

--- src/test/todolist.test.js
+++ src/test/todolist.test.js
@@ -1,39 +1,38 @@
-/* eslint-disable quotes */
-import "jest-localstorage-mock";
-import addData from "../module/addData.js";
-import clearAllData from "../module/clearAllData.js";
-import deletTasksFromLocalSorage from "../module/deleteData.js";
-import { testCurrentValue, testEditValue } from "./testEditData.js";
+import 'jest-localstorage-mock';
+import addData from '../module/addData.js';
+import clearAllData from '../module/clearAllData.js';
+import deletTasksFromLocalSorage from '../module/deleteData.js';
+import { testCurrentValue, testEditValue } from './testEditData.js';
 
-import getdata from "../module/getData.js";
-import testCheckBoxValue from "./testCheckData.js";
+import getdata from '../module/getData.js';
+import testCheckBoxValue from './testCheckData.js';
 
-describe("Test methods", () => {
-  test("should add data to the list", () => {
-    addData("what happened?");
+describe('Test methods', () => {
+  test('should add data to the list', () => {
+    addData('what happened?');
     const data = getdata();
     expect(data.length).toBe(1);
   });
-  test("should edit the data from the list", () => {
-    testCurrentValue("what happened?");
-    testEditValue("reading js");
+  test('should edit the data from the list', () => {
+    testCurrentValue('what happened?');
+    testEditValue('reading js');
     const data = getdata();
-    expect(data[0].description).toBe("reading js");
+    expect(data[0].description).toBe('reading js');
   });
-  test("should check the checkbox of data from the list", () => {
+  test('should check the checkbox of data from the list', () => {
     testCheckBoxValue(true);
     const data = getdata();
     expect(data[0].completed).toBe(true);
   });
 
-  test("should delete the checked data from the list", () => {
+  test('should delete the checked data from the list', () => {
     clearAllData();
     const data = getdata();
     expect(data.length).toBe(0);
   });
 
-  test("should delete data from the list", () => {
-    deletTasksFromLocalSorage("hi");
+  test('should delete data from the list', () => {
+    deletTasksFromLocalSorage('hi');
     const data = getdata();
     expect(data.length).toBe(0);
   });
